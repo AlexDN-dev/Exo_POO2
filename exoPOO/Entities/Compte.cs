9a51025c51dbd9da1da80a6a57c23e2cbff137ea
Alexandre Denève <a.deneve@stag.technofuturtic.education>
2026-03-20 15:21:37 +0100

    add constructor in Courant and Epargne

--- exoPOO/Entities/Compte.cs
+++ exoPOO/Entities/Compte.cs
@@ -8,6 +8,19 @@ public abstract class Compte : IBanker
     public double Solde { get; protected set; }
     public Personne Titulaire { get; set; }
 
+    public Compte(string numero, Personne titulaire)
+    {
+        Numero = numero;
+        Titulaire = titulaire;
+    }
+
+    public Compte(string numero, Personne titulaire, double solde)
+    {
+        Numero = numero;
+        Titulaire = titulaire;
+        Solde = solde;
+    }
+
     public abstract void Retrait(double montant);
 
     protected abstract double CalculInteret();
